e675905edc9e051e053dae71cd9c5996fb45cae2
Martin Kayser <makayser@microsoft.com>
2017-04-15 00:53:58 +0200

    working LUIS version

--- app.js
+++ app.js
@@ -20,41 +20,59 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 // Setup intents
-var intents = new builder.IntentDialog();
-bot.dialog('/', intents);
+//var intents = new builder.IntentDialog();
+//bot.dialog('/', intents);
+
+// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/ce5c4d91-784e-457a-a09b-f27c9fa88d8e?subscription-key=b46b563f753f4af3a63e3441ccfb0417&timezoneOffset=0.0&verbose=true&q=';
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+bot.dialog('/', dialog);
+
+
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-intents.matches(/^change name/i, [
-    function (session) {
-        session.beginDialog('/profile');
-    },
-    function (session, results) {
-        session.send('Ok... Changed your name to %s', session.userData.name);
-    }
-]);
+// intents.matches(/^change name/i, [
+//     function (session) {
+//         session.beginDialog('/profile');
+//     },
+//     function (session, results) {
+//         session.send('Ok... Changed your name to %s', session.userData.name);
+//     }
+// ]);
 
-intents.onDefault([
-    function (session, args, next) {
-        if (!session.userData.name) {
-            session.beginDialog('/profile');
-        } else {
-            next();
-        }
-    },
-    function (session, results) {
-        session.send('Hello %s!', session.userData.name);
-    }
-]);
+// intents.onDefault([
+//     function (session, args, next) {
+//         if (!session.userData.name) {
+//             session.beginDialog('/profile');
+//         } else {
+//             next();
+//         }
+//     },
+//     function (session, results) {
+//         session.send('Hello %s!', session.userData.name);
+//     }
+// ]);
 
-bot.dialog('/profile', [
+// bot.dialog('/profile', [
+//     function (session) {
+//         builder.Prompts.text(session, 'Hi! What is your name?');
+//     },
+//     function (session, results) {
+//         session.userData.name = results.response;
+//         session.endDialog();
+//     }
+// ]);
+
+// Add intent handlers
+dialog.matches('GetJob',[
     function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-        session.endDialog();
+        builder.DialogAction.send('I currently work at Microsoft as data insights consultant.');
     }
 ]);
+dialog.matches('GetLocation', builder.DialogAction.send('Getting Location'));
+dialog.onDefault(builder.DialogAction.send("I'm sorry - I am only allowed to talk about Martin."));
+
